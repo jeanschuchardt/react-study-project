0c951450a86ac3845b38f481f2e15b09b591e34f
Jean <jean@sherpaerp.com>
2022-11-17 11:41:05 -0300

    add callback

--- pics/src/componets/SearchBar.js
+++ pics/src/componets/SearchBar.js
@@ -5,7 +5,8 @@ class SearchBar extends React.Component {
     state = {term: ''}
     onFormSubmit = event => {
         event.preventDefault();
-        console.log(this.state.term)
+        // console.log(this.state.term)
+        this.props.onSubmit(this.state.term);
     }
 
     render() {
