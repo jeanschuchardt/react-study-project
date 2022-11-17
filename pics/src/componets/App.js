224c4449d23215fc36b145fb15917f0776952388
Jean <jean@sherpaerp.com>
2022-11-17 13:53:36 -0300

    add axios to call a api

--- pics/src/componets/App.js
+++ pics/src/componets/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import axios from "axios";
+
 import SearchBar from "./SearchBar";
 
-class App extends React.Component{
-    onSearchSubmit(term){
+class App extends React.Component {
+    onSearchSubmit(term) {
         console.log(term);
+
+        axios.get('https://api.unsplash.com/search/photos',
+            {
+                params: {query: term},
+                headers: {Authorization: 'Client-ID tw4VYePIpJCddXgFQuCKW85emB8kMAJ9wAGX2bN2NW8'}
+
+            })
     }
 
     render() {
