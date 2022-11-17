4d19425e12eb4cb8f5be6929b658b3343ff5d4b7
Jean <jean@sherpaerp.com>
2022-11-17 14:30:11 -0300

    i need to bind to get use the set stage

---
+++ pics/src/components/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+    state = {images: []};
+
+    constructor(props) {
+        super(props);
+        this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    }
+
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization:
+                    'Client-ID tw4VYePIpJCddXgFQuCKW85emB8kMAJ9wAGX2bN2NW8',
+            },
+        });
+
+        this.setState({images: response.data.results});
+        console.log(this.state.images);
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar
+                    onSubmit={this.onSearchSubmit}
+                    guesswhtIam="Im the propsobject"
+                />
+                Found: {this.state.images.length} images
+            </div>
+        );
+    }
+}
+
+export default App;
